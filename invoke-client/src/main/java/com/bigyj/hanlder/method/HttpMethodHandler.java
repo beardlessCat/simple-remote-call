28179f52f17f85f6a1351c1994e50b147cb2547a
lyj <lyj@dareway.com.cn>
2021-07-10 15:49:06 +0800

    ADD： （1）增加超时重试机制

--- invoke-client/src/main/java/com/bigyj/hanlder/method/HttpMethodHandler.java
+++ invoke-client/src/main/java/com/bigyj/hanlder/method/HttpMethodHandler.java
@@ -1,16 +1,22 @@
 package com.bigyj.hanlder.method;
 
 import java.lang.reflect.Type;
+import java.util.concurrent.TimeUnit;
 
 import com.bigyj.common.entity.AccessToken;
+import com.bigyj.common.exception.ApiException;
 import com.bigyj.domain.RequestDomain;
 import com.bigyj.supplier.AccessTokenSupplier;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
@@ -33,7 +39,30 @@ public class HttpMethodHandler implements MethodHandler{
 		if(requestDomain.isWithAccessToken()){
 			accessToken = accessTokenSupplier.get();
 		}
-		return execute(params,accessToken);
+		int tryCount = 0;
+		//超时机制
+		for(;;){
+			try {
+				tryCount++ ;
+				return execute(params, accessToken);
+			}catch (ResourceAccessException e){
+				if(tryCount>requestDomain.getMaxAttempts()){
+					throw new ApiException("00001","接口调用失败！");
+				}
+				try {
+					//线程等待重试
+					TimeUnit.MILLISECONDS.sleep(tryWaitMILLIS);
+				}catch (InterruptedException ex){
+					Thread.currentThread().interrupt();
+					throw ex;
+				}
+				continue;
+			}catch (HttpClientErrorException e) {
+				throw e;
+			} catch (RestClientException e) {
+				throw new ApiException(e.getMessage());
+			}
+		}
 	}
 	Object execute(Object params , AccessToken accessToken) throws ClassNotFoundException {
 		HttpHeaders headers = new HttpHeaders();
@@ -44,6 +73,7 @@ public class HttpMethodHandler implements MethodHandler{
 		HttpEntity entity = new HttpEntity<>(params, headers);
 		Class<?> aClass = Class.forName(returnType.getTypeName());
 		ResponseEntity responseEntity = restTemplate.exchange(requestDomain.getValue(), requestDomain.getMethod(), entity,aClass);
+		HttpStatus statusCode = responseEntity.getStatusCode();
 		Object body = responseEntity.getBody();
 		return body;
 	}
--- invoke-client/src/main/java/com/bigyj/hanlder/method/MethodHandler.java
+++ invoke-client/src/main/java/com/bigyj/hanlder/method/MethodHandler.java
@@ -1,5 +1,6 @@
 package com.bigyj.hanlder.method;
 
 public interface MethodHandler {
+	long tryWaitMILLIS = 1000;
 	Object invoke(Object[] args) throws Throwable;
 }
